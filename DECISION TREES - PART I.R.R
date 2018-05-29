03b740863f1e429b6c092dbc3fca6cb1f3891a48
Devaraj YH <Dev2tg@gmail.com>
2018-05-29 14:18:04 +0530

    Add files via upload

---
+++ DECISION TREES - PART I.R.R
@@ -0,0 +1,18 @@
+setwd("C:/Users/dhirannavara/Desktop/Acadgild")
+
+getwd()
+IrisDecisionTree<-function()
+{
+  str(iris)
+  
+  library("tree")
+  
+  modelRegTree<-tree(Species~Sepal.Length+Sepal.Width+Petal.Length+Petal.Width,data=iris)
+  
+  plot(modelRegTree)
+  text(modelRegTree,pretty = 0,cex=0.75)
+  
+}
+
+
+IrisDecisionTree()
